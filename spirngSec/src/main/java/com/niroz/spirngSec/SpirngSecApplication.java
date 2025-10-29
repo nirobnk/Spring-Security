561cd97791145df1dc22f1128af3592704803da4
nirobnk <eg245158@engug.ruh.ac.lk>
2025-10-29 12:44:34 +0530

    first commit

---
+++ spirngSec/src/main/java/com/niroz/spirngSec/SpirngSecApplication.java
@@ -0,0 +1,13 @@
+package com.niroz.spirngSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngSecApplication.class, args);
+	}
+
+}
---
+++ spirngSec/src/test/java/com/niroz/spirngSec/SpirngSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niroz.spirngSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
